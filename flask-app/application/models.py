b7a8ff320114b971d63cb86be3a8b599b1515818
Nazmeena <111758676+Nazmeena@users.noreply.github.com>
2022-10-26 21:35:28 +0100

    Create models.py

---
+++ flask-app/application/models.py
@@ -0,0 +1,12 @@
+# How your databases are going to look. 
+from application import db 
+
+class Teams(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+class Players(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    position = db.Column(db.String(30))
+    fk_teamid = db.Column(db.Integer, db.ForeignKey('teams.id'))
