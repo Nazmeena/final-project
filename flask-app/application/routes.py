6b288be2e6ad19f3f10e8f4d122ab91f3a81c40f
Nazmeena <111758676+Nazmeena@users.noreply.github.com>
2022-10-27 11:28:53 +0100

    Update routes.py

--- flask-app/application/routes.py
+++ flask-app/application/routes.py
@@ -11,7 +11,7 @@ from application.forms import StudentForm, ClassForm
 def index():
     classes = Classes.query.all()
     students = Students.query.all()
-    return render_template('index.html', title="Student Enrollment", teams=teams, players=players)
+    return render_template('index.html', title="Student Enrollment", classes=classes, students=students)
 
 # CREATE class 
 @app.route('/addclass', methods=['POST', 'GET'])
@@ -38,7 +38,7 @@ def addstudent():
         # todos becomes what we are going to be adding to the database
         students = Students(
             name = form.name.data,
-            set_ = form.set_.data,
+            position = form.position.data,
             # Foreign key as a option to add to the create process. 
             fk_classid = form.fk_classid.data
         )
@@ -78,7 +78,7 @@ def updatestudent(id):
     if form.validate_on_submit():
         # What is put in the form gets ammended to the database
         name.name = form.name.data,
-        name.set_ = form.set_.data,
+        name.position = form.position.data,
         name.fk_classid = form.fk_classid.data
         # Commit the changes
         db.session.commit()
@@ -88,7 +88,7 @@ def updatestudent(id):
     elif request.method == 'GET':
         # Update the form with whats in the database
         form.name.data = name.name 
-        form.set_.data = name.set_
+        form.position.data = name.position
         form.fk_classid.data = name.fk_classid
     # If we go to the url return the template updateplayer.html
     return render_template('updatestudent.html', title='Update the student', form=form)
