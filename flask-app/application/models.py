ac9d27bd711cde3c5a972fe9b9a53a76098a5693
Nazmeena <111758676+Nazmeena@users.noreply.github.com>
2022-10-30 13:44:23 +0000

    Update models.py

--- flask-app/application/models.py
+++ flask-app/application/models.py
@@ -8,5 +8,5 @@ class Classes(db.Model):
 class Students(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-    position = db.Column(db.String(30))
+    course = db.Column(db.String(30))
     fk_classid = db.Column(db.Integer, db.ForeignKey('classes.id'))
