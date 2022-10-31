01121707e808e6fffd0da1dedd98750a48418c6f
Nazmeena <111758676+Nazmeena@users.noreply.github.com>
2022-10-31 09:59:38 +0000

    Update create.sql

--- db/create.sql
+++ db/create.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS classes (
 CREATE TABLE IF NOT EXISTS students (
         id INT PRIMARY KEY AUTO_INCREMENT, 
         name VARCHAR(30),
-        position VARCHAR(30),
+        course VARCHAR(30),
         fk_classid INT, 
         FOREIGN KEY (fk_classid) REFERENCES classes(id)
 );
